05a46181333281aea5f041e3bf33b2d39794618e
Robert Argume <robert@roberts-macbook-pro.local>
2017-12-06 21:33:20 -0500

    Add functionality for the last 4 rows: Add and minus buttons, as well as cancel button

--- quicker-shopping-list/ViewController.swift
+++ quicker-shopping-list/ViewController.swift
@@ -11,40 +11,61 @@ import UIKit
 class ViewController: UIViewController {
 
     let initialQuantityValue = "1"
+    let defaultShoppingListName = "Shopping List"
     
     @IBOutlet weak var shoppingListName: UITextField!
     
     @IBOutlet weak var shoppingListItem: UITextField!
     @IBOutlet weak var quantityLabel: UILabel!
     
-//    @IBOutlet weak var shoppingListItem2: UITextField!
-//    @IBOutlet weak var quantityLabel2: UILabel!
-//    
-//    @IBOutlet weak var shoppingListItem3: UITextField!
-//    @IBOutlet weak var quantityLabel3: UILabel!
-//    
-//    @IBOutlet weak var shoppingListItem4: UITextField!
-//    @IBOutlet weak var quantityLabel4: UILabel!
-//    
-//    @IBOutlet weak var shoppingListItem5: UITextField!
-//    @IBOutlet weak var quantityLabel5: UILabel!
+    @IBOutlet weak var shoppingListItem3: UITextField!
+    
+    @IBOutlet weak var quantityLabel3: UILabel!
+    
+    @IBOutlet weak var shoppingListItem2: UITextField!
+    
+    @IBOutlet weak var quantityLabel2: UILabel!
+    
+    
+    @IBOutlet weak var shoppingListItem4: UITextField!
+    
+    @IBOutlet weak var quantityLabel4: UILabel!
+    
+    @IBOutlet weak var shoppingListItem5: UITextField!
+    
+    
+    @IBOutlet weak var quantityLabel5: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        shoppingListName.text = defaultShoppingListName
         quantityLabel.text = initialQuantityValue
+        quantityLabel2.text = initialQuantityValue
+        quantityLabel3.text = initialQuantityValue
+        quantityLabel4.text = initialQuantityValue
+        quantityLabel5.text = initialQuantityValue
     }
 
     @IBAction func minusButtonPressed(_ sender: UIButton) {
-        var quantity = Int(quantityLabel.text!)!
+        updateQuantityLabel(sender, quantityLabel)
         
-        if sender.tag == 1 {
-             quantity = quantity + 1
-        }
-        else {
-            quantity = quantity > 0 ? quantity - 1 : 0
-        }
-        quantityLabel.text = String(quantity)
-        
+    }
+    
+    @IBAction func quantityButton2Pressed(_ sender: UIButton) {
+        updateQuantityLabel(sender, quantityLabel2)
+    }
+    
+    
+    @IBAction func quantityButton3Pressed(_ sender: UIButton) {
+        updateQuantityLabel(sender, quantityLabel3)
+    }
+    
+    @IBAction func quantityButton4Pressed(_ sender: UIButton) {
+        updateQuantityLabel(sender, quantityLabel4)
+    }
+    
+    @IBAction func quantityButton5Pressed(_ sender: UIButton) {
+        updateQuantityLabel(sender, quantityLabel5)
     }
     
     @IBAction func onTapGestureRecognized(_ sender: Any) {
@@ -54,12 +75,32 @@ class ViewController: UIViewController {
     
    
     @IBAction func onCancelButtonPressed(_ sender: UIButton) {
-        shoppingListName.text = ""
+        shoppingListName.text = defaultShoppingListName
         shoppingListItem.text = ""
+        shoppingListItem2.text = ""
+        shoppingListItem3.text = ""
+        shoppingListItem4.text = ""
+        shoppingListItem5.text = ""
         quantityLabel.text = initialQuantityValue
+        quantityLabel2.text = initialQuantityValue
+        quantityLabel3.text = initialQuantityValue
+        quantityLabel4.text = initialQuantityValue
+        quantityLabel5.text = initialQuantityValue
     }
     
     @IBAction func onSaveButtonPressed(_ sender: UIButton) {
+    }
+    
+    private func updateQuantityLabel(_ sender: UIButton, _ quantityLabel: UILabel!) {
+        var quantity = Int(quantityLabel.text!)!
+        
+        if sender.tag == 1 {
+            quantity = quantity + 1
+        }
+        else {
+            quantity = quantity > 0 ? quantity - 1 : 0
+        }
+        quantityLabel.text = String(quantity)
     }
 }
 
